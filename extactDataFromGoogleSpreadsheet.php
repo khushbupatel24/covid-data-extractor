d258115ffb54aecad1962cf87c7d63bf1be0d86e
Khushbu <pkhush1993@gmail.com>
2020-05-23 19:15:03 +0530

    extract data from state reopen status website

--- extactDataFromGoogleSpreadsheet.php
+++ extactDataFromGoogleSpreadsheet.php
@@ -1,12 +1,14 @@
 <?php
 
-$spreadsheet_url="https://docs.google.com/spreadsheets/d/e/2PACX-1vStD_EMR9El7agVp-Oi6d1c5EMAOYgoYOsSc2xhwzht1ae4Fku7F6zSmF4PB9J_aHA1DAb2PpAelomO/pub?gid=2057441901&single=true&output=csv";
-
 if(!ini_set('default_socket_timeout', 15)) echo "<!-- unable to change socket timeout -->";
+
+// extract "ICU Beds Occupied" tab data from spreadsheet
+$spreadsheet_url="https://docs.google.com/spreadsheets/d/e/2PACX-1vStD_EMR9El7agVp-Oi6d1c5EMAOYgoYOsSc2xhwzht1ae4Fku7F6zSmF4PB9J_aHA1DAb2PpAelomO/pub?gid=2057441901&single=true&output=csv";
+$fp = fopen("ICU Beds Occupied.csv","w");
 
 if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
     $data = fgetcsv($handle);
-    $fp = fopen("ICU Beds Occupied.csv","w");
+    fputcsv($fp, $data);
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         fputcsv($fp, $data);
     }
@@ -14,6 +16,81 @@ if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
     fclose($handle);
 }
 else
-    die("Problem reading csv");
+    die("Problem reading ICU Beds Occupied csv");
+
+// extract "CDC - Gaiting Criteria" tab data from spreadsheet
+$spreadsheet_url="https://docs.google.com/spreadsheets/d/e/2PACX-1vStD_EMR9El7agVp-Oi6d1c5EMAOYgoYOsSc2xhwzht1ae4Fku7F6zSmF4PB9J_aHA1DAb2PpAelomO/pub?gid=852575640&single=true&output=csv";
+
+$fp = fopen("CDC - Gaiting Criteria.csv","w");
+
+if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
+    $data = fgetcsv($handle);
+    fputcsv($fp, $data);
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        fputcsv($fp, $data);
+    }
+    fclose($fp);
+    fclose($handle);
+}
+else
+    die("Problem reading CDC - Gaiting Criteria csv");
 
 print_r("done");
+
+// extract "Population" tab data from spreadsheet
+$spreadsheet_url="https://docs.google.com/spreadsheets/d/e/2PACX-1vStD_EMR9El7agVp-Oi6d1c5EMAOYgoYOsSc2xhwzht1ae4Fku7F6zSmF4PB9J_aHA1DAb2PpAelomO/pub?gid=712897421&single=true&output=csv";
+
+$fp = fopen("Population.csv","w");
+
+if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
+    $data = fgetcsv($handle);
+    fputcsv($fp, $data);
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        fputcsv($fp, $data);
+    }
+    fclose($fp);
+    fclose($handle);
+}
+else
+    die("Problem reading Population csv");
+
+print_r("done");
+
+// extract "Data for website" tab data from spreadsheet
+$spreadsheet_url="https://docs.google.com/spreadsheets/d/e/2PACX-1vStD_EMR9El7agVp-Oi6d1c5EMAOYgoYOsSc2xhwzht1ae4Fku7F6zSmF4PB9J_aHA1DAb2PpAelomO/pub?gid=237779988&single=true&output=csv";
+
+$fp = fopen("Data for website.csv","w");
+
+if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
+    $data = fgetcsv($handle);
+    fputcsv($fp, $data);
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        fputcsv($fp, $data);
+    }
+    fclose($fp);
+    fclose($handle);
+}
+else
+    die("Problem reading Data for website csv");
+
+print_r("done");
+
+// extract "Interventions & Measures - Data" tab data from spreadsheet
+$spreadsheet_url="https://docs.google.com/spreadsheets/d/e/2PACX-1vStD_EMR9El7agVp-Oi6d1c5EMAOYgoYOsSc2xhwzht1ae4Fku7F6zSmF4PB9J_aHA1DAb2PpAelomO/pub?gid=1459999830&single=true&output=csv";
+
+$fp = fopen("Interventions & Measures - Data.csv","w");
+
+if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
+    $data = fgetcsv($handle);
+    fputcsv($fp, $data);
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        fputcsv($fp, $data);
+    }
+    fclose($fp);
+    fclose($handle);
+}
+else
+    die("Problem reading Interventions & Measures - Data csv");
+
+print_r("done");
+
---
+++ extractStateReopenMapData.php
@@ -0,0 +1,61 @@
+<?php
+
+$url = "https://www.nytimes.com/interactive/2020/us/states-reopen-map-coronavirus.html";
+
+$dom = new DOMDocument;
+$dom->loadHTMLFile($url);
+$finder = new DomXPath($dom);
+
+$date = '';
+foreach ($dom->getElementsByTagName('time') as $ele) {
+    $date = $ele->nodeValue;
+    break;
+}
+
+$classname = "g-state g-cat-reopening";
+$nodes = $finder->query("//*[contains(@class, '$classname')]");
+
+$fp = fopen("state data -${date}.csv","w");
+fputcsv($fp, ['status', 'state']);
+
+$reopeningStates = [];
+foreach ($nodes as $key => $ele) {
+    foreach ($ele->getElementsByTagName('div') as $divTag) {
+        fputcsv($fp, ['reopening', trim($divTag->nodeValue)]);
+        break;
+    }
+}
+
+$classname = "g-state g-cat-regional";
+$nodes = $finder->query("//*[contains(@class, '$classname')]");
+
+foreach ($nodes as $key => $ele) {
+    foreach ($ele->getElementsByTagName('div') as $divTag) {
+        fputcsv($fp, ['regional', trim($divTag->nodeValue)]);
+        break;
+    }
+}
+
+$classname = "g-state g-cat-soon";
+$nodes = $finder->query("//*[contains(@class, '$classname')]");
+
+foreach ($nodes as $key => $ele) {
+    foreach ($ele->getElementsByTagName('div') as $divTag) {
+        fputcsv($fp, ['opening-soon', trim($divTag->nodeValue)]);
+        break;
+    }
+}
+
+$classname = "g-state g-cat-shutdown-restricted";
+$nodes = $finder->query("//*[contains(@class, '$classname')]");
+
+$restrictedStates = [];
+foreach ($nodes as $key => $ele) {
+    foreach ($ele->getElementsByTagName('div') as $divTag) {
+        fputcsv($fp, ['restricted', trim($divTag->nodeValue)]);
+        break;
+    }
+}
+
+fclose($fp);
+exit();

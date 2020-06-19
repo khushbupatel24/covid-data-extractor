b2023dbe6e2180af8e10bd204e30741cb0189b50
Khushbu <pkhush1993@gmail.com>
2020-06-19 10:03:33 +0530

    stop fetching population data from spreadsheet

--- extractDataFromGoogleSpreadsheet.php
+++ extractDataFromGoogleSpreadsheet.php
@@ -36,22 +36,22 @@ if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
 print_r("done");
 
 // extract "Population" tab data from spreadsheet
-$spreadsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vStD_EMR9El7agVp-Oi6d1c5EMAOYgoYOsSc2xhwzht1ae4Fku7F6zSmF4PB9J_aHA1DAb2PpAelomO/pub?gid=712897421&single=true&output=csv";
-
-$fp = fopen(dirname(__FILE__)."/data/Population.csv", "w");
-
-if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
-    $data = fgetcsv($handle);
-    fputcsv($fp, $data);
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        fputcsv($fp, $data);
-    }
-    fclose($fp);
-    fclose($handle);
-} else
-    die("Problem reading Population csv");
-
-print_r("done");
+//$spreadsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vStD_EMR9El7agVp-Oi6d1c5EMAOYgoYOsSc2xhwzht1ae4Fku7F6zSmF4PB9J_aHA1DAb2PpAelomO/pub?gid=712897421&single=true&output=csv";
+//
+//$fp = fopen(dirname(__FILE__)."/data/Population.csv", "w");
+//
+//if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
+//    $data = fgetcsv($handle);
+//    fputcsv($fp, $data);
+//    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+//        fputcsv($fp, $data);
+//    }
+//    fclose($fp);
+//    fclose($handle);
+//} else
+//    die("Problem reading Population csv");
+//
+//print_r("done");
 
 // extract "Data for website" tab data from spreadsheet
 $spreadsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vStD_EMR9El7agVp-Oi6d1c5EMAOYgoYOsSc2xhwzht1ae4Fku7F6zSmF4PB9J_aHA1DAb2PpAelomO/pub?gid=237779988&single=true&output=csv";

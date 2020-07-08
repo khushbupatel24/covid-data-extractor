4ae6f8d8374b89386f737c5f3a77f99d00097ef2
Khushbu <pkhush1993@gmail.com>
2020-07-08 22:54:37 +0530

    extract current unemployment claims by states data

--- extractWeeklyUnemploymentClaimsByStatesData.php
+++ extractWeeklyUnemploymentClaimsByStatesData.php
@@ -2,23 +2,27 @@
 
 $url = "https://oui.doleta.gov/unemploy/csv/ar539.csv";
 
+$date = date("d-m-Y");
+$fp = fopen(dirname(__FILE__) . "/data/unemploymentClaims-${date}.csv", "w");
+
 if (($handle = fopen($url, "r")) !== false) {
     $firstRow = true;
     $aData = array();
     while (($data = fgetcsv($handle)) !== false) {
         if ($firstRow) {
             $headers = array_values($data);
-            $headers = [$headers[0], $headers[3], $headers[4]];
+            fputcsv($fp, [$headers[0], $headers[3], $headers[4]]);
             $firstRow = false;
         } else {
             $record = array_values($data);
             $date = date_create_from_format("m/d/Y", $data[3]);
-            if($date > date_create_from_format("m/d/Y", '01/01/2020')) {
-                $aData[] = array_combine($headers, [$record[0], $record[3], $record[4]]);
+            if ($date > date_create_from_format("m/d/Y", '01/01/2020')) {
+                fputcsv($fp, [$record[0], $record[3], $record[4]]);
             }
         }
     }
     fclose($handle);
 }
 
-print_r(json_encode($aData));
+fclose($fp);
+exit();
